7b18eb62c12859ed94493b3934382c84b28e60d9
Stijn Meijerink <stijn.meijerink@gmail.com>
2019-12-07 17:53:38 +0100

    Quick 'fix' for weird finger tables

--- src/main/java/org/distributed/systems/ChordStart.java
+++ src/main/java/org/distributed/systems/ChordStart.java
@@ -8,12 +8,13 @@ public class ChordStart {
 
     public static final int STANDARD_TIME_OUT = 10000;
     // FIXME 160 according to sha-1 but this is the max_length of a java long..
-    public static int M = 100; // Number of bits in key nodeId's
+    public static int M = 10; // Number of bits in key nodeId's
     public static final long AMOUNT_OF_KEYS = Math.round(Math.pow(2, M));
 
     public static void main(String[] args) {
         // Create actor system
         ActorSystem system = ActorSystem.create("ChordNetwork"); // Setup actor system
+        // FIXME we want to read this value from the config but it doesn't work (yet)
         M = system.settings().config().getInt("myapp.mBits"); // Override mBits based on config
 
         system.actorOf(Props.create(NodeActor.class), "ChordActor");
